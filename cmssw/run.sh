3264e7adfd25058371fd78b9a9efae58ee72c755
Andres Rios Tascon <ariostas@gmail.com>
2024-09-30 13:56:47 -0400

    Switch to 29834.1 workflow for CMSSW check (#16)
    * Updated Dockerfile
    
    * Switched to new CMSSW workflow
    
    * Fixed typo
    
    * Use step2 file with pileup
    
    * Run over 75 events
    
    * Update detector
    
    * Go back to 100 events

--- cmssw/run.sh
+++ cmssw/run.sh
@@ -19,7 +19,10 @@ git sparse-checkout set .gitignore .clang-format .clangtidy
 git fetch SegLink refs/pull/${PR_NUMBER}/head:SegLink_cmssw
 git checkout SegLink_cmssw
 git fetch SegLink $TARGET_BRANCH
-git cms-addpkg RecoTracker/LST RecoTracker/LSTCore Configuration/ProcessModifiers RecoTracker/ConversionSeedGenerators RecoTracker/FinalTrackSelectors RecoTracker/IterativeTracking HeterogeneousCore/AlpakaInterface
+git cms-addpkg RecoTracker/LST RecoTracker/LSTCore \
+  Configuration/ProcessModifiers Configuration/PyReleaseValidation \
+  RecoTracker/ConversionSeedGenerators RecoTracker/FinalTrackSelectors RecoTracker/IterativeTracking \
+  HeterogeneousCore/AlpakaInterface
 # Temporarily merge target branch
 git config user.email "gha@example.com" && git config user.name "GHA"
 git merge --no-commit --no-ff SegLink/${TARGET_BRANCH} || (echo "***\nError: There are merge conflicts that need to be resolved.\n***" && false)
@@ -29,20 +32,41 @@ scram b -j 4
 # Download data files
 git clone --branch add_t33_maps https://github.com/SegmentLinking/RecoTracker-LSTCore.git RecoTracker/LSTCore/data
 echo "Starting LST test..."
-cmsDriver.py step3 -s RAW2DIGI,RECO:reconstruction_trackingOnly,VALIDATION:@trackingOnlyValidation,DQM:@trackingOnlyDQM --conditions auto:phase2_realistic_T21 --datatier GEN-SIM-RECO,DQMIO -n 100 --eventcontent RECOSIM,DQM --geometry Extended2026D88 --era Phase2C17I13M9 --procModifiers trackingLST,trackingIters01 --nThreads 4 --no_exec
-sed -i "28i process.load('Configuration.StandardSequences.Accelerators_cff')\nprocess.load('HeterogeneousCore.AlpakaCore.ProcessAcceleratorAlpaka_cfi')\n" step3_RAW2DIGI_RECO_VALIDATION_DQM.py
+cmsDriver.py step3 \
+  -s RAW2DIGI,RECO:reconstruction_trackingOnly,VALIDATION:@trackingOnlyValidation,DQM:@trackingOnlyDQM \
+  --conditions auto:phase2_realistic_T33 \
+  --datatier GEN-SIM-RECO,DQMIO \
+  -n 100 \
+  --eventcontent RECOSIM,DQM \
+  --geometry Extended2026D110 \
+  --era Phase2C17I13M9 \
+  --procModifiers trackingIters01,trackingLST \
+  --accelerators cpu \
+  --nThreads 4 \
+  --filein file:/data2/segmentlinking/step2_29834.1_100Events.root \
+  --fileout file:step3_out.root \
+  --no_exec
 if [[ "$LOW_PT" == "true" ]]; then
   sed -i "30i process.load('RecoTracker.LST.lstModulesDevESProducer_cfi')\nprocess.load('RecoTracker.LST.lstProducer_cfi')\nprocess.lstModulesDevESProducer.ptCutLabel = '0.6'\nprocess.lstProducer.ptCutLabel = '0.6'\nprocess.lstProducer.ptCut = 0.6" step3_RAW2DIGI_RECO_VALIDATION_DQM.py
 fi
-sed -i "s|fileNames = cms.untracked.vstring('file:step3_DIGI2RAW.root')|fileNames = cms.untracked.vstring('file:/data2/segmentlinking/step2_21034.1_100Events.root')|" step3_RAW2DIGI_RECO_VALIDATION_DQM.py
 echo "Setting up siteconf..."
 git clone https://github.com/cms-sw/siteconf.git
 sed -i '/<prefer ipfamily="0"\/>/,/<backupproxy url="http:\/\/cmsbproxy\.fnal\.gov:3128"\/>/d' siteconf/local/JobConfig/site-local-config.xml
 export SITECONFIG_PATH=$PWD/siteconf/local
-echo "Running 21034.1 workflow..."
+echo "Running 29834.1 (+LST) workflow..."
 cmsRun step3_RAW2DIGI_RECO_VALIDATION_DQM.py
-cmsDriver.py step4 -s HARVESTING:@trackingOnlyValidation+@trackingOnlyDQM --conditions auto:phase2_realistic_T21 --mc  --geometry Extended2026D88 --scenario pp --filetype DQM --era Phase2C17I13M9 -n 100 --no_exec
-sed -i "s|fileNames = cms.untracked.vstring('file:step4_RECO.root')|fileNames = cms.untracked.vstring('file:step3_RAW2DIGI_RECO_VALIDATION_DQM_inDQM.root')|" step4_HARVESTING.py
+cmsDriver.py step4 \
+  -s HARVESTING:@trackingOnlyValidation+@trackingOnlyDQM \
+  --conditions auto:phase2_realistic_T33 \
+  --mc \
+  --geometry Extended2026D110 \
+  --scenario pp \
+  --filetype DQM \
+  --era Phase2C17I13M9 \
+  -n 100 \
+  --filein file:step3_out_inDQM.root \
+  --fileout file:step4_out.root \
+  --no_exec
 cmsRun step4_HARVESTING.py
 mv DQM_V0001_R000000001__Global__CMSSW_X_Y_Z__RECO.root this_PR.root
 rm step3_*.root
@@ -57,7 +81,7 @@ eval `scramv1 runtime -sh`
 # Recompile CMSSW in case anything changed in the headers
 scram b clean
 scram b -j 4
-echo "Running 21034.1 workflow..."
+echo "Running 29834.1 (+LST) workflow..."
 cmsRun step3_RAW2DIGI_RECO_VALIDATION_DQM.py
 cmsRun step4_HARVESTING.py
 mv DQM_V0001_R000000001__Global__CMSSW_X_Y_Z__RECO.root target_branch.root
